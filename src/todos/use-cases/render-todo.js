21bb479c18a21581b0e40d38de1e8bf4d11038b5
Sergio Barreras <sergiobg.isc@gmail.com>
2023-11-07 17:24:41 -0700

    limpiar element en render todo

--- src/todos/use-cases/render-todo.js
+++ src/todos/use-cases/render-todo.js
@@ -11,6 +11,9 @@ let element;
 export const renderTodos = (elementId, todos = []) => {
   if (!element) element = document.querySelector(elementId);
   if (!element) throw new Error(`Element ${elementId} not found.`);
+
+  element.innerHTML = "";
+
   todos.forEach((todo) => {
     element.append(createTodoHTML(todo));
   });
