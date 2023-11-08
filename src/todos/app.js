39ea47366955cfaf01da4a6f309b5ecd77cf078e
Sergio Barreras <sergiobg.isc@gmail.com>
2023-11-08 07:37:32 -0700

    conteo de pendientes implementado

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,12 +1,13 @@
 import html from "./app.html?raw";
 import todoStore, { Filters } from "../store/todo.store";
-import { renderTodos } from "./use-cases";
+import { renderTodos, renderPending } from "./use-cases";
 
 const ElementsIds = {
   TodoList: ".todo-list",
   NewTodoInput: "#new-todo-input",
   ClearCompleted: ".clear-completed",
   TodoFilters: ".filtro",
+  PendingCountLabel: "#pending-count",
 };
 
 /**
@@ -18,6 +19,11 @@ export const App = (elementId) => {
   const displayTodos = () => {
     const todos = todoStore.getTodos(todoStore.getCurrentFilter());
     renderTodos(ElementsIds.TodoList, todos);
+    updatePendingCount();
+  };
+
+  const updatePendingCount = () => {
+    renderPending(ElementsIds.PendingCountLabel);
   };
 
   (() => {
